6dbc7d2d1c0865e3e03234f4e48f3fb0ea38772e
mrafiwahyu10 <125549959+mrafiwahyu10@users.noreply.github.com>
2023-06-07 10:43:19 +0700

    Tugas Harian 2

---
+++ app/src/main/java/com/rafi/daftartemanrafi/data_teman.kt
@@ -0,0 +1,15 @@
+package com.rafi.daftartemanrafi
+
+class data_teman {
+    var nama: String? = null
+    var alamat: String? = null
+    var no_hp: String? = null
+    var key: String? = null
+
+    constructor(nama: String?, no_hp: String?) {
+        this.nama = nama
+        this.alamat = alamat
+        this.no_hp = no_hp
+    }
+
+}
